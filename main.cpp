8c1696686f914e42d8837219ac87d420374e7c83
linhaojun857 <lhj13401792631@163.com>
2023-10-07 23:33:18 +0800

    server-template

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <cooper/net/AppTcpServer.hpp>
+#include <dbng.hpp>
+#include <mysql.hpp>
+
+using namespace cooper;
+using namespace ormpp;
+
+#define TEST 1
+
+struct Person {
+    int id{};
+    std::string name;
+    std::optional<int> age;
+};
+REFLECTION(Person, id, name, age)
+
+int main() {
+    AppTcpServer server;
+    dbng<mysql> mysql;
+    bool ret;
+    ret = mysql.connect("172.18.48.1", "root", "20030802", "test");
+    if (!ret) {
+        LOG_ERROR << "connect mysql failed";
+    }
+    mysql.create_datatable<Person>(ormpp_auto_key{"id"});
+    server.registerProtocolHandler(TEST, [&mysql](const TcpConnectionPtr& conn, const json& j) {
+        Person p;
+        p.name = j["name"];
+        p.age = j["age"];
+        auto ret = mysql.insert(p);
+        if (ret) {
+            LOG_INFO << "insert success";
+        } else {
+            LOG_INFO << "insert failed";
+        }
+        conn->send("hello, world");
+    });
+    server.start();
+    return 0;
+}
